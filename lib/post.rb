39468f0f7a0ff986b1070b49c18a64a35c8608f1
Adam Urbina <adamurbina@gmail.com>
2018-06-10 11:25:50 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,14 @@
 class Post
 
-    
+    attr_accessor :title
+    @@all = []
 
     def initialize(title)
         @title = title
         @@all << title
+    end
+
+    def all
+        @@all
     end
 end
