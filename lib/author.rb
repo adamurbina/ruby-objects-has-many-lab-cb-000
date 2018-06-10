36622f8623249ae8cbe34bdb8f57e0334e533316
Adam Urbina <adamurbina@gmail.com>
2018-06-10 11:23:50 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,8 @@ class Author
 
     attr_accessor :name
 
+    @@all = []
+
     def initialize(name)
         @name = name
         @posts = []
@@ -10,11 +12,16 @@ class Author
     def add_post(post)
         post.author = self
         @posts << post
+        @@all << post
     end
 
     def add_post_by_title(title)
         new_post = Post.new(title)
         add_post(new_post)
+    end
+
+    def self.post_count
+        @@all.size
     end
 
 end
