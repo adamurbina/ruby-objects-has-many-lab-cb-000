2cfd6a16240f179d8487109678fefd715c185e99
Adam Urbina <adamurbina@gmail.com>
2018-06-10 11:21:43 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,6 +9,12 @@ class Author
 
     def add_post(post)
         post.author = self
+        @posts << post
+    end
+
+    def add_post_by_title(title)
+        new_post = Post.new(title)
+        add_post(new_post)
     end
 
 end
